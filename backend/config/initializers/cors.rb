78461936d3e05d3244ad4e4e532b5d0fd51262bc
wscindy <wscindy.lee@gmail.com>
2025-11-11 20:13:23 +0800

    Fix CORS problem in Vue

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -14,3 +14,17 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # 允許來自 Vue 前端的請求
+    origins 'http://localhost:5173', 'http://127.0.0.1:5173'
+    
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
